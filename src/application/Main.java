28b11641b8299a54144f7bf0525381823fc55261
Cristian Vitali <hp@DESKTOP-ETSKT4S>
2019-04-12 19:38:29 +0200

    Modifica intefaccia

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("DatabaseApp");
-        primaryStage.setScene(new Scene(root, 600, 200));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
